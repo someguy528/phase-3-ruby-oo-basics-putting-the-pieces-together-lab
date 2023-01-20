9baa849970b5de5bd5b4a448f4ee4131779cf27c
Brandon Castro <castroyou999@gmail.com>
2023-01-19 21:56:09 -0500

    commit tree aa635b2511fcdf79923c5a525c90863ebf0cfc3b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
-
-
+class Book
+    # attr_accessor :title
+    # attr_accessor :author
+    # attr_accessor :page_count
+    # attr_accessor :genre
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
